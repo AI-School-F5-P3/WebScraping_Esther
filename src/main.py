c0234f8f668be3c4c9480b9be9bdffcb8446a399
EstherTapias <esthertpc93@gmail.com>
2024-07-26 22:18:53 +0200

    Limpio datos del scraping y lo conecto a la db, añado logging, configuro archivo .env

--- src/main.py
+++ src/main.py
@@ -4,6 +4,50 @@ from bs4 import BeautifulSoup
 from colorama import Fore #para añadir colores a los comentarios 
 import time
 from unidecode import unidecode #para estandarizar el lenguaje(limpiamos los acentos)
+from dotenv import load_dotenv
+import os #librería para interactuar con otros archivos
+from sqlalchemy import create_engine
+import psycopg2
+import logging
+'''Para testear que ciertos bloques de código se ejecuten o no. 5 tipos de mensaje:
+Debug=10(testear cierta parte de nuestro código),
+Info=20(se encuentra en el flujo normal de la app)
+Warning=30(alerta)
+Error=40(mensaje de errores)
+Critical=50(alerta de que el programa ya no puede continuar)'''
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename= 'WebScraping_Esther/log/scraping.log',
+                    filemode='a',
+                    encoding='utf-8' )
+
+load_dotenv() #Para conectar con la db, y nos traemos la información de las variables creadas en el archivo .env
+USER = os.getenv('USER')
+PWD = os.getenv('PASSWORD')
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+DATABASE = os.getenv('DATABASE')
+
+try:
+    conexion = psycopg2.connect(
+        dbname=DATABASE,
+        user=USER,
+        password=PWD,
+        host=HOST,
+        port=PORT
+    )
+    print(f'{Fore.BLUE}Conexión exitosa a la base de datos: {DATABASE}!{Fore.RESET}')
+    logging.info(f'{Fore.BLUE}Conexión exitosa a la base de datos: {DATABASE}!{Fore.RESET}')
+    conexion.close()
+except Exception as e:
+    logging.error(f"Conexión fallida: {e}")
+
+
+connection_string = f'postgresql://{USER}:{PWD}@{HOST}:{PORT}/{DATABASE}'
+engine = create_engine(connection_string)
+table_name= 'XYZCorp_Datos'
+
 
 page_website= 'https://quotes.toscrape.com/page/'
 about_website= 'https://quotes.toscrape.com/author/'
@@ -11,7 +55,14 @@ about_website= 'https://quotes.toscrape.com/author/'
 def about_scraping(autor_limpieza):
     author_website=f'{about_website}{autor_limpieza}'
     print(f"{Fore.GREEN}Descargando autor {autor_limpieza}: {author_website}{Fore.RESET}")
+    logging.info(f"{Fore.GREEN}Descargando autor {autor_limpieza}: {author_website}{Fore.RESET}")
     contenido_author = requests.get(author_website)
+    if contenido_author.status_code == 404:
+        logging.error(f"Página para {autor_limpieza} no encontrada (404). Termina scrape.")
+        return None  
+    elif contenido_author.status_code != 200:
+        logging.error(f"Error en la página para {autor_limpieza}. Status code: {contenido_author.status_code}. Termina scrape.")
+        return None
     soup = BeautifulSoup(contenido_author.text, 'html.parser')
     nombre=soup.find('h3', class_= 'author-title').text.strip()
     nacimiento_fecha=soup.find('span', class_= 'author-born-date').text.strip()
@@ -34,12 +85,13 @@ def quote_scraping():
     while True: #bucle para actualizar la pagina web si se añade contenido en base a si aumenta el número de páginas en la web
         website = f'{page_website}{page_number}'#formato
         print(f"{Fore.MAGENTA}Descargando página {page_number}: {website}{Fore.RESET}")
+        logging.info(f"{Fore.MAGENTA}Descargando página {page_number}: {website}{Fore.RESET}")
         
         try:
             contenido = requests.get(website)
-            contenido.raise_for_status()  
+            contenido.raise_for_status() #Estado de la respuesta  
         except requests.RequestException as e:
-            print(f"{Fore.RED}Error: {e}{Fore.RESET}")
+            logging.error(f"{Fore.RED}Error: {e}{Fore.RESET}")
             break
 
         soup = BeautifulSoup(contenido.text, 'html.parser')
@@ -47,6 +99,7 @@ def quote_scraping():
 
         if not data:
             print(f"{Fore.YELLOW}Datos no encontrados en página {page_number}. Termina el scrape.{Fore.RESET}")
+            logging.info(f"{Fore.YELLOW}Datos no encontrados en página {page_number}. Termina el scrape.{Fore.RESET}")
             break
 
         for div in data:
@@ -65,7 +118,6 @@ def quote_scraping():
                 'Cita':cita,
                 'Autor':autor,
                 'Tag':', '.join(tags), #','.join(tags) para unir todos los tagas en un str y separarlos con ,
-                'Nombre': autor_info['Nombre'],
                 'Fecha_Nacimiento':autor_info['Fecha_Nacimiento'],
                 'Lugar_nacimiento':autor_info['Lugar_nacimiento'],
                 'Descripcion':autor_info['Descripcion']
@@ -77,6 +129,17 @@ def quote_scraping():
         time.sleep(2)#tiempo que pasa entre una vuelta y otra que da el while
 
     df = pd.DataFrame(quote_data)
-    print(df)
-        
-quote_scraping()
+    return df
+
+
+if __name__ == "__main__":
+    df_final = quote_scraping()
+    # Enviar el DataFrame a la base de datos
+    try:
+        df_final.to_sql(table_name, engine, if_exists='replace', index=False) #
+        print(f"{Fore.BLUE}DataFrame enviado a la tabla {table_name} en la base de datos {DATABASE}{Fore.RESET}")
+        logging.info(f"{Fore.BLUE}DataFrame enviado a la tabla {table_name} en la base de datos {DATABASE}{Fore.RESET}")
+    except Exception as e:
+        logging.error(f"{Fore.RED}Error al enviar el DataFrame a la base de datos: {e}{Fore.RESET}")
+
+logging.info(df_final)
